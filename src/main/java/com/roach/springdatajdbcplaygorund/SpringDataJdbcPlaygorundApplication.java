e7a2eced4dd3321c3a78bdf24619559f9d21547c
roach <roach@cleaninglab.co.kr>
2021-07-12 22:20:16 +0900

    setup: init

---
+++ src/main/java/com/roach/springdatajdbcplaygorund/SpringDataJdbcPlaygorundApplication.java
@@ -0,0 +1,13 @@
+package com.roach.springdatajdbcplaygorund;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJdbcPlaygorundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJdbcPlaygorundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/roach/springdatajdbcplaygorund/SpringDataJdbcPlaygorundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roach.springdatajdbcplaygorund;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJdbcPlaygorundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
